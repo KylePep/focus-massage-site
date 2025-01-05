32f67b02ed478a2672c5249413370690b4436899
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-05 14:53:16 -0700

    setting up project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="sticky-top">
+    Header
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { AppState } from './AppState'
+
+const appState = computed(() => AppState)
+</script>
+
+<style>
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+  --color-fm-orange: rgb(249, 170, 140);
+  --color-fm-pink: rgb(250, 227, 233);
+  --color-fm-teal: rgb(83, 146, 161);
+  --color-fm-blue: rgb(0, 78, 97);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue'
-import './style.css'
-import './index.css'
+// @ts-ignore
 import App from './App.vue'
+import { router } from "./router.js"
+import { registerGlobalComponents } from './registerGlobalComponents.js'
 
-createApp(App).mount('#app')
+const root = createApp(App)
+async function init() {
+  await registerGlobalComponents(root)
+  root
+    .use(router)
+    .mount('#app')
+}
+init()
