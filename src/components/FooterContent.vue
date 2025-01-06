b29776a1b377abe41d5fabc53f957f6e226f0156
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-05 17:23:08 -0700

    styling on footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,9 +1,94 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 
 <template>
-  Footer
+  <div class="text-xs lg:text-lg">
+    <div class="bg-gray-900 px-6 lg:px-40 grid grid-cols-4 gap-1 gap-y-8 lg:gap-20 text-white/90 py-12">
+
+      <section class="col-span-4 lg:col-span-1 space-y-4 h-full lg:flex flex-col justify-around">
+        <div class="font-bold text-base lg:text-lg">
+          Get In Touch
+        </div>
+        <ul class="space-y-2">
+          <li class="font-bold text-base">Address: <span class="font-normal text-sm">address spelt out</span></li>
+          <li class="font-bold text-base">Phone: <span class="font-normal text-sm">xxx-xxx-xxxx</span></li>
+          <li class="font-bold text-base">Email: <span class="font-normal text-sm">email@emai.com</span></li>
+        </ul>
+        <div class="flex gap-4">
+          <button class="bg-gray-600 rounded-full h-8 w-8">1</button>
+          <button class="bg-gray-600 rounded-full h-8 w-8">2</button>
+          <button class="bg-gray-600 rounded-full h-8 w-8">3</button>
+          <button class="bg-gray-600 rounded-full h-8 w-8">4</button>
+
+        </div>
+      </section>
+
+      <section class="col-span-4 lg:col-span-1 space-y-4 lg:flex flex-col justify-around">
+        <div class="font-bold text-base lg:text-lg">
+          Recent Posts
+        </div>
+        <ul class="flex-1 flex flex-col gap-4">
+          <li class="grid grid-cols-4">
+            <i class="bg-blue-500 me-4"></i>
+            <div class="col-span-3 text-xs lg:text-sm">
+              <p>Minor Surgeries on the foot</p>
+              <p class="text-gray-400">March 01, 2019</p>
+            </div>
+          </li>
+
+          <li class="grid grid-cols-4">
+            <i class="bg-blue-500 me-4"></i>
+            <div class="col-span-3 text-xs lg:text-sm">
+              <p>Assistive Device Program</p>
+              <p class="text-gray-400">March 01, 2019</p>
+            </div>
+          </li>
+
+          <li class="grid grid-cols-4">
+            <i class="bg-blue-500 me-4"></i>
+            <div class="col-span-3 text-xs lg:text-sm">
+              <p>Initial plus treatment</p>
+              <p class="text-gray-400">March 01, 2019</p>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="col-span-4 lg:col-span-1 space-y-4 lg:flex flex-col justify-around">
+        <div class="font-bold text-base lg:text-lg">
+          Customer Service
+        </div>
+        <ul class="space-y-1 lg:text-sm">
+          <li>Physiotherapy</li>
+          <li>Massage Therapy</li>
+          <li>Vehicle Injury Treatment</li>
+          <li>Acupuncture</li>
+          <li>Neurological Rehabilitiation</li>
+          <li>Custom-Made Orthotics</li>
+        </ul>
+      </section>
+
+      <section class="col-span-4 lg:col-span-1 space-y-4 lg:flex flex-col justify-around">
+        <div class="font-bold text-base lg:text-lg">
+          Subscribe To Us
+        </div>
+        <p>Get Business news, tip and solutions to your problems from our experts.</p>
+        <div class="flex">
+          <input type="text" class="w-full" name="" id="">
+          <button class="bg-blue-500 px-8">></button>
+        </div>
+
+      </section>
+
+
+    </div>
+    <div class="bg-black py-2 lg:py-4 text-center text-white">
+      Copyright 2024 by FocusMassage. All rights reserved
+    </div>
+  </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style scoped></style>
