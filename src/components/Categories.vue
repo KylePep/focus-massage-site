f1c994cfae1e29abe43e952d9710abe1c39d93c3
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-06 09:10:33 -0700

    Created CategoryCard

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,52 +1,50 @@
-<script setup></script>
+<script setup>
+import CategoryCard from './CategoryCard.vue'</script>
 
 
 <template>
 
   <div class=" grid grid-cols-3 gap-4">
 
-    <section
-      class="group card-bg col-span-3 lg:col-span-1 flex flex-col justify-around text-center space-y-4 p-4 hover:bg-blue-700/20 duration-1000 relative overflow-hidden">
-      <i class="h-16 w-16 mx-auto text-fm-blue">
+    <CategoryCard>
+      <template #icon>
         <FMIcon />
-      </i>
-      <h2 class="font-semibold">Massage Therapy</h2>
-      <p class="text-sm">Massage therapy is the manipulation of the soft tissues of the body including muscles,
+      </template>
+      <template #title>
+        Massage Therapy
+      </template>
+      <template #content>
+        Massage therapy is the manipulation of the soft tissues of the body including muscles,
         connective tissue,
-        tendons, ligaments and joints.</p>
-      <i class="absolute -right-16 -bottom-16 h-64 w-64 text-transparent group-hover:text-black/10 duration-1000">
-        <FMIcon />
-      </i>
-      <div class="absolute bottom-0 left-0 w-0 group-hover:w-full border-b-4 border-blue-500 duration-1000"></div>
-    </section>
+        tendons, ligaments and joints.
+      </template>
+    </CategoryCard>
 
-    <section
-      class="group card-bg col-span-3 lg:col-span-1 flex flex-col justify-around text-center space-y-4 p-4 hover:bg-blue-700/20 duration-1000 relative overflow-hidden">
-      <i class="h-16 w-16 mx-auto text-fm-blue">
+    <CategoryCard>
+      <template #icon>
         <FMIcon />
-      </i>
-      <h2 class="font-semibold">Physio Therapy</h2>
-      <p class="text-sm">Physiotherapists spend years studying how the body works, how injuries impact performance & how
-        to recover and repair injured tissues.</p>
-      <i class="absolute -right-16 -bottom-16 h-64 w-64 text-transparent group-hover:text-black/10 duration-1000">
-        <FMIcon />
-      </i>
-      <div class="absolute bottom-0 left-0 w-0 group-hover:w-full border-b-4 border-blue-500 duration-1000"></div>
-    </section>
+      </template>
+      <template #title>
+        Physio Therapy
+      </template>
+      <template #content>
+        Physiotherapists spend years studying how the body works, how injuries impact performance & how
+        to recover and repair injured tissues.
+      </template>
+    </CategoryCard>
 
-    <section
-      class="group card-bg col-span-3 lg:col-span-1 flex flex-col justify-around text-center space-y-4 p-4 hover:bg-blue-700/20 duration-1000 relative overflow-hidden">
-      <i class="h-16 w-16 mx-auto text-fm-blue">
+    <CategoryCard>
+      <template #icon>
         <FMIcon />
-      </i>
-      <h2 class="font-semibold">Acupuncture</h2>
-      <p class="text-sm">Acupuncture is a form of alternative medicine in which thin needles are inserted into the body.
-        It is a key component of medicine.</p>
-      <i class="absolute -right-16 -bottom-16 h-64 w-64 text-transparent group-hover:text-black/10 duration-1000">
-        <FMIcon />
-      </i>
-      <div class="absolute bottom-0 left-0 w-0 group-hover:w-full border-b-4 border-blue-500 duration-1000"></div>
-    </section>
+      </template>
+      <template #title>
+        Acupuncture
+      </template>
+      <template #content>
+        Acupuncture is a form of alternative medicine in which thin needles are inserted into the body.
+        It is a key component of medicine.
+      </template>
+    </CategoryCard>
 
   </div>
 </template>
