318082937748a7fca515019c8bd36ade3797107d
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-06 12:49:30 -0700

    found a good carousel library

--- src/components/PricingCarousel.vue
+++ src/components/PricingCarousel.vue
@@ -1,8 +1,62 @@
-<script setup></script>
+<script setup>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css/skyblue';
+
+</script>
 
 
 <template>
-  NewVueTemplate
+
+  <Splide aria-label="My Favorite Images" data-splide='{"type":"loop","perPage":3}'>
+    <SplideSlide>
+      <div class="m-4">
+        1
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="m-4">
+        2
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="m-4">
+        3
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="m-4">
+        4
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="m-4">
+        5
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="m-4">
+        6
+        <img
+          src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Sample 1">
+      </div>
+    </SplideSlide>
+  </Splide>
 </template>
 
 
