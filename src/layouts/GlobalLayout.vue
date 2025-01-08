20cd627f2a969fa99795f63d07b0a4c62ad63050
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-08 09:46:32 -0700

    min-h-screen prevents flickering

--- src/layouts/GlobalLayout.vue
+++ src/layouts/GlobalLayout.vue
@@ -53,7 +53,7 @@ onUnmounted(() => {
       <slot name="header"></slot>
     </header>
 
-    <main class="flex-1 mt-10 lg:mt-14">
+    <main class="flex-1 mt-10 lg:mt-14 min-h-screen">
       <slot />
     </main>
 
