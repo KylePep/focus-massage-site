cc7f7143d6881c4ae0f01a266d2554d63b7a1c1e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-20 11:27:38 -0700

    fixed colors on ResponsiveNavLink

--- src/components/Nav/ResponsiveNavLink.vue
+++ src/components/Nav/ResponsiveNavLink.vue
@@ -5,8 +5,8 @@ const props = defineProps(['href', 'active']);
 
 const classes = computed(() =>
     props.active
-        ? 'border-fm-orange text-start text-base font-medium bg-teal-500 focus:outline-none focus:bg-teal-300 focus:border-fm-pink transition duration-150 ease-in-out'
-        : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium hover:bg-teal-500 hover:border-teal-300 focus:outline-none focus:bg-teal-300 focus:border-teal-300 transition duration-150 ease-in-out'
+        ? 'border-fm-orange text-start text-base font-medium bg-blue-200 focus:outline-none focus:bg-blue-100 focus:border-fm-pink transition duration-150 ease-in-out'
+        : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium hover:bg-blue-200 hover:border-blue-100 focus:outline-none focus:bg-blue-100 focus:border-blue-100 transition duration-150 ease-in-out'
 );
 </script>
 
