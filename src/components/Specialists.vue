593ffe33d1139071eb344aff7d5c5ddccef51fe7
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-20 11:25:06 -0700

    off image scroll in, hover border effect

--- src/components/Specialists.vue
+++ src/components/Specialists.vue
@@ -17,11 +17,16 @@ import SectionTitle from "./SectionTitle.vue";
         </template>
       </SectionTitle>
 
-      <section class="grid grid-cols-3 ">
-        <div class="relative col-span-3 lg:col-span-1 lg:col-start-2 p-4 text-center space-y-2 group">
+      <section class="grid grid-cols-3">
+        <div class="relative col-span-3 lg:col-span-1 lg:col-start-2 py-4 text-center space-y-2 group overflow-hidden">
           <img class="rounded-sm" src="/img/DanaHead.jpg" alt="Dana Whalen Massage Therapist">
           <h1 class="font-light">Registered Massage Therapist</h1>
           <h2 class="text-lg font-semibold">Dana Whalen</h2>
+          <div class="absolute bottom-1/4 -left-96 group-hover:left-0 duration-1000 bg-blue-500 rounded-r-full">
+            <ul class="flex space-x-8 px-8 py-2 font-bold text-white">
+              <li>6 years experience</li>
+            </ul>
+          </div>
           <div class="absolute bottom-0 left-0 w-0 group-hover:w-full border-b-2 border-blue-500 duration-1000"></div>
         </div>
       </section>
