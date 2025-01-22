e4abf8bd969bc849a7b6e4c7db5505baaebcbf13
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-01-22 11:46:21 -0700

    ServiceDetails page can access individual items from data

--- src/pages/ServiceDetailsPage.vue
+++ src/pages/ServiceDetailsPage.vue
@@ -1,11 +1,22 @@
 <script setup>
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router";
+import { SERVICE_ITEMS } from "../data/index.js";
+
+const services = ref(SERVICE_ITEMS)
+
 const route = useRoute()
+
+const serviceData = computed(() => {
+  const serviceId = String(route.params.serviceId);
+  return services.value[serviceId];
+})
 </script>
 
 
 <template>
   <div class="space-y-16 lg:space-y-32">
-
+    {{ serviceData }}
   </div>
 </template>
 
